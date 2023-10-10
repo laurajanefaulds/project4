78c05708670ee0fb22943170c81902011d74449f
rafiatazeen <131704075+rafiatazeen@users.noreply.github.com>
2023-10-10 19:55:16 -0400

    Add files via upload

---
+++ diabetes schema.sql
@@ -0,0 +1,17 @@
+create table diabetes (
+	age real,
+	hypertension integer,
+	heart_disease integer,
+	bmi	real,
+	HbA1c_level real,
+	blood_glucose_level integer,
+	diabetes integer,
+	gender_Female integer,
+	gender_Male integer,
+	smoking_history_No_Info integer,
+	smoking_history_current integer,
+	smoking_history_former integer,
+	smoking_history_never integer,
+	smoking_history_occasional integer);
+	
+	select * from diabetes
